ce757dda3ee50cf3cc0f3d61f1d8f900f7da87ba
Vinay Kumar Singh <vinaysingh43@gmail.com>
2024-09-20 10:40:06 +0530

    Add files via upload

---
+++ chatbot_db.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 20, 2024 at 06:34 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chatbot_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatbot_responses`
+--
+
+CREATE TABLE `chatbot_responses` (
+  `id` int(11) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `answer` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `chatbot_responses`
+--
+
+INSERT INTO `chatbot_responses` (`id`, `question`, `answer`) VALUES
+(1, 'What are your working hours?', 'We are open from 9 AM to 5 PM, Monday to Friday.'),
+(2, 'What is your name?', 'I am your virtual assistant.'),
+(3, 'What is CodeIgniter?', 'CodeIgniter is a powerful PHP framework for building web applications.'),
+(4, 'How do I install CodeIgniter?', 'You can install CodeIgniter using Composer by running \"composer create-project codeigniter4/appstarter\".'),
+(5, 'What is Bootstrap?', 'Bootstrap is a popular front-end framework for developing responsive and mobile-first websites.'),
+(6, 'How do I use jQuery?', 'jQuery is a fast, small, and feature-rich JavaScript library. You can include it in your project by adding the jQuery CDN link.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chatbot_responses`
+--
+ALTER TABLE `chatbot_responses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chatbot_responses`
+--
+ALTER TABLE `chatbot_responses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
